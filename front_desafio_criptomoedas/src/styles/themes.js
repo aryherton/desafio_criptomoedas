87ad90f2267c9f72ab3b47ebfc1042b883850560
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-12 12:53:41 -0300

    feat: created file "themes.js" in folder "./src/styles"

---
+++ front_desafio_criptomoedas/src/styles/themes.js
@@ -0,0 +1,27 @@
+const Dark = {
+    colors: {
+        pageLoginBG: '#211856',
+        formLoginBG: '#181E30',
+        formBorder: '#46535D',
+        buttonFollowBG: '#FAFBFC',
+        fontNameRepos: '#0366D6',
+        marksStacks: '#4F5D95',
+        marksStackHTML: '#E34C26',
+        placeholderInput: '#98a7b7',
+    }
+};
+
+const Light = {
+    colors: {
+        pageSearchBG: '#FAFAFA',
+        pageHomeBG: '#FFFFFF',
+        buttonSearchBG: '#46535D',
+        buttonFollowBG: '#FAFBFC',
+        fontNameRepos: '#0366D6',
+        marksStacks: '#4F5D95',
+        marksStackHTML: '#E34C26',
+        placeholderInput: '#98a7b7',
+    }
+};
+
+export { Dark, Light };
