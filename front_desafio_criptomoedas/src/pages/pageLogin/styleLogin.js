871267c62d4e7a6b880d6b809ed2a24895a11bce
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-12 12:51:13 -0300

    feat: created file "styleLogin.js" in folder "./src/pages/pageLogin"

---
+++ front_desafio_criptomoedas/src/pages/pageLogin/styleLogin.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyleLoginWrapper = styled.div`
+  background-image: linear-gradient(to right, #181E30, #2a076b, #181E30);
+  width: 100%;
+  height: 100vh;
+`;
+
+export default StyleLoginWrapper;
