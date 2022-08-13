b24866fb1463171de7b903db70d0d61d4fdc30b6
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-13 19:21:45 -0300

    feat: created file "schema_user.go" in folder "./dabase/models"

---
+++ back_desafio_criptomoedas/database/models/schema_user.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID          primitive.ObjectID `bson:"_id" json:"id" structs:"id"`
+	Name        string             `bson:"name" json:"name" binding:"required"`
+	Email       string             `bson:"email" json:"email" binding:"required"`
+	Password    string             `bson:"password" json:"password" binding:"required"`
+	Name_Crypto string             `bson:"name_crypto" json:"name_crypto"`
+}
+
+type Users []*User
