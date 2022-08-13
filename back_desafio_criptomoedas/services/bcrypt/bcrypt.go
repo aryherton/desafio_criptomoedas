adedd7042d0c3c910588e6f71d72dbe35784376a
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-13 19:32:39 -0300

    feat: created file "bcrypt.go" in folder "./services/bcrypt"

---
+++ back_desafio_criptomoedas/services/bcrypt/bcrypt.go
@@ -0,0 +1,16 @@
+package services
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string, value_const int) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), value_const)
+
+	return string(bytes)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
