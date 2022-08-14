fab2bf598b6565ed76353a517d11e8257c9aa45c
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-14 08:29:21 -0300

    feat: import package "bcryt" and "token" in file "./database/models"

--- back_desafio_criptomoedas/database/models/model_user.go
+++ back_desafio_criptomoedas/database/models/model_user.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	db "github.com/aryherton/desafio_criptomoedas/database/config"
+	s "github.com/aryherton/desafio_criptomoedas/services/bcrypt"
+	s_token "github.com/aryherton/desafio_criptomoedas/services/token"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -21,6 +23,31 @@ func Create(user User) error {
 	}
 
 	return nil
+}
+
+func Login(email string, password string) (string, error) {
+	var user User
+	var err error
+	var pwd bool
+
+	user, err = Read_By_Email(email)
+	if err != nil {
+		return "", err
+	}
+
+	pwd = s.CheckPasswordHash(password, user.Password)
+
+	if !pwd {
+		return "", err
+	}
+
+	token := s_token.New_jwt_token().Create_token(email)
+
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
 
 func Read() (Users, error) {
@@ -45,6 +72,20 @@ func Read() (Users, error) {
 	}
 
 	return users, nil
+}
+
+func Read_By_Email(email string) (User, error) {
+	var user User
+
+	filter := bson.M{"email": email}
+
+	err := collection.FindOne(ctx, filter).Decode(&user)
+
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
 
 func Update(user User_update, user_id string) error {
