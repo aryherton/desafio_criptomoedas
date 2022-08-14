a6acb10755135bd22fba1a8d0498e6fe1653d025
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-14 09:55:05 -0300

    faet: created file "styleHeader.js" in folder "./src/components/header"

---
+++ front_desafio_criptomoedas/src/components/header/styleHeader.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyleHeaderWrapper = styled.header`
+    display: flex;
+    width: 100%;
+    height: 60px;
+    background-color: blue;
+`;
+
+export default StyleHeaderWrapper;
