fce812a93046e59d2213dbca423c5ccd999e9b44
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-14 09:54:28 -0300

    feat: created file "Header.jsx" in folder "./src/components/header"

---
+++ front_desafio_criptomoedas/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import StyleHeaderWrapper from './styleHeader';
+
+function Header() {
+  return (
+    <StyleHeaderWrapper>
+      <div>data</div>
+      <div>info user and exit</div>
+    </StyleHeaderWrapper>
+  );
+}
+
+export default Header;
