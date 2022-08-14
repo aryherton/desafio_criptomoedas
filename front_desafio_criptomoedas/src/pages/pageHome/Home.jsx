f21861471aa0b03c0b426e632e29f6014998df24
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-14 09:56:06 -0300

    feat: import component "Heder" in file "./src/pages/pageHome/Home.jsx"

--- front_desafio_criptomoedas/src/pages/pageHome/Home.jsx
+++ front_desafio_criptomoedas/src/pages/pageHome/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import StyleHomeWrapper from './styleHome';
+import Header from '../../components/header/Header';
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <StyleHomeWrapper>
+      <section id="sectionMenuV">
+        <div id="infoMenu">
+          Infomaçẽos
+        </div>
+        <div id="divMenuV">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </div>
+      </section>
+      <aside id="asideHome">
+        <Header />
+      </aside>
+    </StyleHomeWrapper>
   );
 }
 
