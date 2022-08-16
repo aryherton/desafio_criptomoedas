f7fb2acc462dbc0535e8e79e203c6e9cf24b492b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-16 14:22:27 -0300

    refactor: change in style for file "Header.jsx" in folder "./src/components/header"

--- front_desafio_criptomoedas/src/components/header/Header.jsx
+++ front_desafio_criptomoedas/src/components/header/Header.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import Context from '../../context/DataContext';
 
 import StyleHeaderWrapper from './styleHeader';
+import Logo from '../../img/logo.png';
 
 function Header() {
+  const { dataUser } = useContext(Context);
+  const emailUser = JSON.parse(localStorage.getItem('user')).email;
+  const user = dataUser && dataUser.filter((obj) => obj.email === emailUser)[0].name;
+  
   return (
     <StyleHeaderWrapper>
-      <div>data</div>
-      <div>info user and exit</div>
+      <div id="divImgLogo">
+        <img src={ Logo } alt="logo" />
+      </div>
+      <div id="divInfoUser">
+        <div>
+          <p>Olá,</p>
+          <p>{user && user}</p>
+        </div>
+        <div id="buttonSair">
+          <button type="button" className="btn btn-primary">Sair</button>
+        </div>
+      </div>
     </StyleHeaderWrapper>
   );
 }
--- front_desafio_criptomoedas/src/components/header/styleHeader.js
+++ front_desafio_criptomoedas/src/components/header/styleHeader.js
@@ -1,10 +1,43 @@
 import styled from 'styled-components';
 
 const StyleHeaderWrapper = styled.header`
+    background-color: #0B0E14;
     display: flex;
+    justify-content: space-between;
+    height: 90px;
     width: 100%;
-    height: 60px;
-    background-color: blue;
+    #divImgLogo {
+        margin-left: 80px;
+        width: 90px;
+        height: 90px;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    #divInfoUser {
+        color: #C2C8D1;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 150px;
+        height: 100%;
+        font-size: 18px;
+        margin-right: 80px;
+        #buttonSair {
+            width: 60px;
+            height: 30px;
+            background-color: #F9B50A;
+            border-radius: 80px;
+            button {
+                width: 100%;
+                height: 100%;
+                background: none;
+                border: none;
+                border-radius: 80px;
+            }
+        }
+    }
 `;
 
 export default StyleHeaderWrapper;
