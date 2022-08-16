16c5e685af26170850e9b6e063a4721aec79a92d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-15 22:20:35 -0300

    feat: created file "arrCoins.js" in folder "./src/services/help"

---
+++ front_desafio_criptomoedas/src/services/help/arrCoins.js
@@ -0,0 +1,34 @@
+export const ARR_COINS = [
+    {
+        name: 'ADA',
+        img: 'https://bit.ly/3QJ7cyi'
+    },
+    {
+        name: 'BTC',
+        img: 'https://bit.ly/3JUXsie',
+    },
+    {
+        name: 'ETH',
+        img: 'https://bit.ly/3PpJmXs',
+    },
+    {
+        name: 'DOGE',
+        img: 'https://bit.ly/3QtXOPg',
+    },
+    {
+        name: 'DOT',
+        img: 'https://bit.ly/3w6E9gp',
+    },
+    {
+        name: 'SOL',
+        img: 'https://bit.ly/3SRnQNY',
+    },
+    {
+        name: 'USDC',
+        img: 'https://bit.ly/3Pwg1dD',
+    },
+    {
+        name: 'XRP',
+        img: 'https://bit.ly/3Pwg6Ot',
+    },
+];
