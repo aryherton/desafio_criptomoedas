985d19c950a0ef013a5b8b2297b99136149ae9f7
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-17 18:51:14 -0300

    refactor: inserted "favoriteCoin" in file "./src/components/header/Header.jsx"

--- front_desafio_criptomoedas/src/components/header/Header.jsx
+++ front_desafio_criptomoedas/src/components/header/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Context from '../../context/DataContext';
 
@@ -6,19 +7,44 @@ import StyleHeaderWrapper from './styleHeader';
 import Logo from '../../img/logo.png';
 
 function Header() {
+  let favoriteCoin = '';
+  const nav = useNavigate();
   const { dataUser } = useContext(Context);
-  const emailUser = JSON.parse(localStorage.getItem('user')).email;
-  const user = dataUser && dataUser.filter((obj) => obj.email === emailUser)[0].name;
+  const getStorage = JSON.parse(localStorage.getItem('user'));
+  let userName = '';
+
+  if (getStorage) {
+    const emailUser = getStorage.email;
+    userName = dataUser && dataUser.filter((obj) => obj.email === emailUser)[0];
+    favoriteCoin = userName.name_crypto;
+  
+    let userStorage = JSON.parse(localStorage.getItem('user'));
+    userStorage.name = userName.name;
+    userStorage.id = userName.id;
+    userStorage.name_crypto = userName.name_crypto;
+
+    localStorage.setItem('user', JSON.stringify(userStorage));
+  } else {
+    nav('/');
+  }
   
   return (
     <StyleHeaderWrapper>
       <div id="divImgLogo">
         <img src={ Logo } alt="logo" />
       </div>
+      {favoriteCoin
+        && (
+        <div id="favoriteCoin">
+          <p>Sua moeda favorita: </p>
+          <h2>{favoriteCoin}</h2>
+          </div>
+        )
+      }
       <div id="divInfoUser">
         <div>
           <p>Olá,</p>
-          <p>{user && user}</p>
+          <p>{userName && userName.name}</p>
         </div>
         <div id="buttonSair">
           <button type="button" className="btn btn-primary">Sair</button>
