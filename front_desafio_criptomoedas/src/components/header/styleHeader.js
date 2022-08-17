3ffd305bc31e10f67b748790301878d5f62160fe
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-17 18:52:01 -0300

    feat: inserted css id "#favoriteCoin" in file "./src/components/header/styleHeader.js"

--- front_desafio_criptomoedas/src/components/header/styleHeader.js
+++ front_desafio_criptomoedas/src/components/header/styleHeader.js
@@ -4,6 +4,7 @@ const StyleHeaderWrapper = styled.header`
     background-color: #0B0E14;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     height: 90px;
     width: 100%;
     box-shadow: 0px 0px 30px rgba(0,0,0,0.59);
@@ -14,6 +15,17 @@ const StyleHeaderWrapper = styled.header`
         img {
             width: 100%;
             height: 100%;
+        }
+    }
+    #favoriteCoin {
+        color: #F9B50A;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        p {
+            color: #C2C8D1;
+            margin-bottom: 10px;
         }
     }
     #divInfoUser {
