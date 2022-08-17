29dbb20582d5d4de578dc41d697ba92003bc4232
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-16 22:02:27 -0300

    feat: imported component "TableInfoCoins" in file "./src/pages/pageHome/Home.jsx"

--- front_desafio_criptomoedas/src/pages/pageHome/Home.jsx
+++ front_desafio_criptomoedas/src/pages/pageHome/Home.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Context from '../../context/DataContext';
+import Header from '../../components/header/Header';
+import TableInfoCoins from '../../components/tableInfo/TableInfoCoins';
+import Aside from '../../components/aside/Aside';
+
+import { getAllUser } from '../../services/requestApi';
+import { getDataApiMercBTC } from '../../services/apiMercadoBtc';
 
 import StyleHomeWrapper from './styleHome';
-import Header from '../../components/header/Header';
 
 function Home() {
+  const nav = useNavigate();
+  const {
+    dataUser,
+    setDataUser,
+    dataCryptonApi,
+    setDataCryptApi,
+  } = useContext(Context);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!user) {
+      nav('/');
+    }
+
+    if (!dataUser || !dataCryptonApi) {
+      (async () => {
+        const arrUsers = await getAllUser('user/', user.token);
+        const dataCryptonApi = await getDataApiMercBTC();
+
+        setDataCryptApi(dataCryptonApi);
+        setDataUser(arrUsers);
+      })();
+    }
+  }, []);
+
   return (
     <StyleHomeWrapper>
-      <section id="sectionMenuV">
-        <div id="infoMenu">
-          Infomaçẽos
-        </div>
-        <div id="divMenuV">
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-          </ul>
-        </div>
-      </section>
-      <aside id="asideHome">
-        <Header />
-      </aside>
+      <Header />
+      <main>
+        <section>
+          <TableInfoCoins />
+        </section>
+        <Aside />
+      </main>
     </StyleHomeWrapper>
   );
 }
