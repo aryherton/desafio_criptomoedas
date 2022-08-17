db27d3f503a1da3029b77f76f51205e1fc075f3c
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-16 22:00:50 -0300

    feat: created file "Aside.jsx" in folder "./src/components/aside"

---
+++ front_desafio_criptomoedas/src/components/aside/Aside.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { nanoid } from 'nanoid';
+
+import Context from '../../context/DataContext'; 
+import { ARR_COINS } from '../../services/help/arrCoins';
+import StyleAsideWrapper from './styleAside';
+
+function Aside() {
+  const{ dataUser } = useContext(Context);
+  const coins = ARR_COINS.map((obj) => ({ name: obj.name, score: 0 }));
+  const arrCoinsScore = dataUser && coins.map((obj) => {
+    dataUser.forEach((e) => {
+      if (e.name_crypto === obj.name) {
+        obj.score += 1;
+      }
+    });  
+    return obj;
+  })
+    .sort((a, b) => b.score - a.score);
+  
+  return (
+    <StyleAsideWrapper>
+      <div></div>
+      <h2>Favoritas</h2>
+      {arrCoinsScore && <div id="coinsPopular">
+        <ul>
+          { arrCoinsScore.map((obj, index) => (
+            <li id="sortCoins" key={nanoid}>
+              <span>{ index + 1 }° - </span>
+              <span>{ obj.name }</span>
+            </li>
+            )) }
+        </ul>
+      </div> }
+    </StyleAsideWrapper>
+  );
+}
+
+export default Aside;
