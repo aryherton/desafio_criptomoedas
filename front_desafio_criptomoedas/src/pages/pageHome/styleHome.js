1a967b4af2f57e48b13f2a8a7ef0c2c277721186
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-16 22:50:51 -0300

    refactor: inserted tags "width" and "height" for tag "main" in file "./src/pages/pageHome/styleHome.js"

--- front_desafio_criptomoedas/src/pages/pageHome/styleHome.js
+++ front_desafio_criptomoedas/src/pages/pageHome/styleHome.js
@@ -5,6 +5,8 @@ const StyleHomeWrapper = styled.div`
   height: 100vh;
   background-color: #171E29;
   main {
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: flex-end;;
     align-items: center;
