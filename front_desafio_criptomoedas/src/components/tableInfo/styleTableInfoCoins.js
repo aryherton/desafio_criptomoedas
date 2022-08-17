dbf4a5ae693b4a32ffcbfb4b2d1ac1575594c26b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-16 22:49:22 -0300

    refactor: change value tag "height" for id "#thTable" in file "./src/components/tableInfo/styleTableInfoCoins.js"

--- front_desafio_criptomoedas/src/components/tableInfo/styleTableInfoCoins.js
+++ front_desafio_criptomoedas/src/components/tableInfo/styleTableInfoCoins.js
@@ -6,20 +6,18 @@ const StyleTableInfoCoins = styled.div`
   table {
     background-color: yellow;
     width: 100%;
-    height: 100%;
     text-align: center;
   }
   .thTable {
     width: 110px;
-    height: 30px;
-    padding-top: 10px;
-    padding-bottom: 10px;
+    height: 45px;
+    padding-top: 20px;
     background-color: #393c3f;
     color: #fff;
   }
   #thName {
     width: 90px;
-    height: 30px;
+    height: 25px;
     padding-left: 25px;
     background-color: #393c3f;
     color: #fff;
@@ -27,7 +25,7 @@ const StyleTableInfoCoins = styled.div`
   .tr-even {
     background-color: #0B0E14;
     width: 100%;
-    height: 35px;
+    height: 40px;
     color: #F9B50A;
     font-size: 14px;
   }
@@ -35,17 +33,16 @@ const StyleTableInfoCoins = styled.div`
     background-color: #1b1d23;
     color: #fff;
     width: 100%;
-    height: 35px;
-    padding-bottom: 5px;
+    height: 40px;
     font-size: 14px;
   }
   #tdFirst {
     width: 10%;
-    height: 100%;
+    height: 40px;
   }
   .imgAndNameCoin {
     width: 90px;
-    height: 30px;
+    height: 25px;
     display: flex;
     justify-content: space-around;
     img {
